bc7a799e22cf43ecb84db2390e3e1b82bc3e664e
Shakeel Haider <43218437+shkhaider2015@users.noreply.github.com>
2021-07-20 15:06:42 +0500

    fgdfg

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,34 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import { shallow, ShallowWrapper } from 'enzyme';
 
 test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
 });
+
+describe("Timer Test", () => {
+  let wrapper: ShallowWrapper;
+  beforeEach(()=> {
+    wrapper = shallow(<App />);
+  })
+
+  test("renders the title of Timer", () => {
+    expect(wrapper.find("h1").text()).toContain("Timer App using TDD");
+  })
+
+  test("render buttons with text of START/STOP", () => {
+    expect(wrapper.find('.start-btn').text()).toBe('START')
+    expect(wrapper.find('.stop-btn').text()).toBe('STOP')
+  })
+
+  test("render the initial value of state in a div", () => {
+    expect(wrapper.find("#timer-value").text()).toBe('0')
+  })
+
+  test("render the click event of START/STOP button", () => {
+    
+    wrapper.find(".start-btn").simulate('click')
+
+  })
+
+})
--- src/App.tsx
+++ src/App.tsx
@@ -3,22 +3,23 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [timerValue, setTimerValue] = React.useState<number>(0)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ display : 'grid', placeContent : 'center' }} >
+      <h1> Timer App using TDD </h1>
+      <div id="timer-value" >
+        {
+          timerValue
+        }
+      </div>
+      <div>
+        <button className="start-btn" >
+              START
+        </button>
+        <button className="stop-btn" >
+              STOP
+        </button>
+      </div>
     </div>
   );
 }
